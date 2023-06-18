e0425d0dc1f3fbbec4a17f3954c9c170424d71cc
Made Marshall Vira Deva <96987996+marshallvd@users.noreply.github.com>
2023-06-19 00:12:23 +0800

    TUGAS

---
+++ app/src/main/java/marshall/melajah/myapplication/PemasukanActivity.kt
@@ -0,0 +1,11 @@
+package marshall.melajah.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PemasukanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pemasukan)
+    }
+}
